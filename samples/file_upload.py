31533bc15bb6f1d1dbe6f81dc51b00bef4759d66
Takafumi Iju <ijufumi@gmail.com>
2020-01-17 18:22:56 +0900

    modified

--- samples/file_upload.py
+++ samples/file_upload.py
@@ -1,10 +1,11 @@
-import request
+import requests
 
 url = ''
 file_name = ''
 file_path = ''
 def upload():
-    res = request.post(url, files={'file': (file_name, open(file_path, 'rb')})
+    res = requests.post(url, files={'file': (file_name, open(file_path, 'rb'))})
+    print(res)
 
 if __name__ == '__main__':
     upload()
