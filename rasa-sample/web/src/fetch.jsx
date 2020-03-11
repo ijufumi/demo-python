f896020e9b4149b51c1391aafcf140f256f52490
Takafumi Iju <ijufumi@gmail.com>
2020-03-11 18:17:46 +0900

    Add test code for fetch API

---
+++ rasa-sample/web/src/fetch.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const hostTest = "http://fsra-regbot-pilot-test-971690635.ap-southeast-1.elb.amazonaws.com";
+const hostLocal = "http://localhost:8080";
+
+class FetchTest extends React.Component {
+    async handleFetch() {
+        const params = {
+            username: "sample@test.com",
+            password: "password",
+        };
+        const result = await fetch(hostTest + "/api/admin/v1/login", {
+            credentials: "same-origin",
+            method: "post",
+            body: JSON.stringify(params)
+        })
+            .then(response => {
+                const headers = {};
+                for (let [key, value] of response.headers) {
+                    headers[key] = value;
+                }
+                return {
+                  headers: headers,
+                  body: response.json(),
+                };
+            })
+            .catch(error => console.log("Error", error)); // eslint-disable-line no-console
+
+        console.log(result);
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={()=>this.handleFetch()}>Fetch</button>
+            </div>
+        );
+    }
+}
+
+export default FetchTest;
--- rasa-sample/web/src/index.jsx
+++ rasa-sample/web/src/index.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Widget } from 'rasa-webchat';
+import FetchTest from './fetch';
 
-
+//http://fsra-regbot-pilot-597364771.ap-southeast-1.elb.amazonaws.com:5005
 ReactDOM.render(
-    <Widget
-        initPayload={"/get_started"}
-        socketUrl={"http://localhost:5005"}
-        socketPath={"/socket.io/"}
-        customData={{"language": "en"}}
-        title={"Title"}
-        customComponent={(messageData) => (
-            <div><div>{messageData.key1}</div><div>{messageData.key2}</div></div>
-        ) }
-    />,
+    <div>
+        <Widget
+            initPayload={"/get_started"}
+            socketUrl={"http://localhost:5005"}
+            socketPath={"/socket.io/"}
+            customData={{"language": "en","questionId": 7101,"applicationId": 1}}
+            title={"Title"}
+            customComponent={(messageData) => (
+                <div><div>{messageData.key1}</div><div>{messageData.key2}</div></div>
+            ) }
+        />
+        <FetchTest/>
+    </div>,
     document.querySelector('.container')
 );
